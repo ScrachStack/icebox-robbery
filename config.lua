c104569fb27a1e9deacffa1c8233bebf206fdaa9
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-08 20:45:35 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ Config.Framework = {
     Amount = '1',
     CashToBeGiven = '',
     FrameworkType = '', -- 'qb', 'esx legacy', or 'nd'
+    UseOxInventory = true, -- [true/false]
     JewelCases = { -- this is how you can edit the coords there vector4
         { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62, looted = false }, -- Case 01
         { x = -1238.6, y = -804.88, z = 17.84, heading = 7.62, looted = false }, -- Case 02
