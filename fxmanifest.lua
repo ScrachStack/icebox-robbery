671c73ea1312f1bc0b0ef971769e9b574fd75c53
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-08 18:48:29 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Zaps'
+version '1.0.0'
+
+client_scripts {
+    'client/*',
+}
+server_script 'server/*'
+shared_script 'config.lua'
+shared_script '@es_extended/imports.lua'
+
