fc36099994f19c8f31ea719c0f10a98b01d5e221
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-08 18:53:37 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Framework = {
+    ItemName = 'Jewel',
+    Amount = '1',
+    CashToBeGiven = '',
+    FrameworkType = '', -- 'qb', 'esx legacy', or 'nd'
+    JewelCases = { -- this is how you can edit the coords there vector4
+        { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62, looted = false }, -- Case 01
+        { x = -1238.6, y = -804.88, z = 17.84, heading = 7.62, looted = false }, -- Case 02
+        { x = -1238.45, y = -806.59, z = 17.84, heading = 197.62, looted = false }, -- Case 03
+
+        -- Add more jewel case positions here
+    }
+}
