761f89088876b25ade03aa85a30911f72aa6c16f
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-30 02:45:44 -0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -3,29 +3,10 @@ local blip = nil
 
 
 
-function CreateStoreBlip(x, y, z, sprite, color, name)
-    blip = AddBlipForCoord(x, y, z)
-    SetBlipSprite(blip, sprite)
-    SetBlipColour(blip, color)
-    SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(name)
-    EndTextCommandSetBlipName(blip)
-end
-
-
 Citizen.CreateThread(function()
-    -- Coordinates for the store
-    local storeX = -1237.12
-    local storeY = -802.53
-    local storeZ = 17.84
+    local storeBlipConfig = Config.Framework.StoreBlip
 
-
-    local blipSprite = 363 
-    local blipColor = 0 
-    local blipName = "Icebox"
-
-    CreateStoreBlip(storeX, storeY, storeZ, blipSprite, blipColor, blipName)
+    CreateStoreBlip(storeBlipConfig.x, storeBlipConfig.y, storeBlipConfig.z, storeBlipConfig.sprite, storeBlipConfig.color, storeBlipConfig.name)
 end)
 
 
@@ -75,90 +56,13 @@ AddEventHandler('robberyInProgress', function()
 end)
 
 
---   -- Define the jewel cases' positions and rotations within the icebox MLO
--- local jewelCases = {
---     { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62 }, -- Case 01
---     { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62 }, -- Case 01
-
---     -- Add more jewel case positions here
--- }
-
--- -- Function to play the breaking glass animation for the player's ped
--- function PlayBreakingGlassAnimation()
---     local playerPed = PlayerPedId()
---     RequestAnimDict("missheist_jewel@hacking")
-
---     while not HasAnimDictLoaded("missheist_jewel@hacking") do
---         Citizen.Wait(0)
---     end
-
---     TaskPlayAnim(playerPed, "missheist_jewel@hacking", "hacking_loop", 8.0, -8.0, -1, 49, 0, false, false, false)
---     Citizen.Wait(5000) -- Adjust the duration of the animation as needed
-
---     ClearPedTasks(playerPed)
--- end
-
--- -- Function to give the player a jewel item
--- function GiveJewelItem()
---     TriggerServerEvent('zaps:bankRobbery', 1000)
--- end
-
--- Citizen.CreateThread(function()
---     while true do
---         Citizen.Wait(0)
---         local playerPed = PlayerPedId()
---         local playerCoords = GetEntityCoords(playerPed)
-
---         if IsRobbing then -- Check if the player is currently robbing
---             for _, case in ipairs(jewelCases) do
---                 local caseCoords = vector3(case.x, case.y, case.z)
---                 local distance = #(playerCoords - caseCoords)
-
---                 if distance < 2.0 then
---                     DisplaySmashGlass()
---                     if IsControlJustReleased(0, 38) then -- Check if the player pressed the "E" key
---                         PlayBreakingGlassAnimation()
---                         GiveJewelItem()
---                     end
---                 end
---             end
---         end
---     end
--- end)
 
 
 
--- Define the jewel cases' positions and rotations within the icebox MLO
-local jewelCases = {
-    { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62, looted = false }, -- Case 01
-    { x = -1238.6, y = -804.88, z = 17.84, heading = 7.62, looted = false }, -- Case 02
-    { x = -1238.45, y = -806.59, z = 17.84, heading = 197.62, looted = false }, -- Case 02
 
-    -- Add more jewel case positions here
-}
 
--- Function to play the breaking glass animation for the player's ped
-function PlayBreakingGlassAnimation()
-    local playerPed = PlayerPedId()
-    RequestAnimDict("missheist_jewel")
 
-    while not HasAnimDictLoaded("missheist_jewel@hacking") do
-        Citizen.Wait(0)
-    end
 
-    TaskPlayAnim(playerPed, "missheist_jewel@hacking", "hacking_loop", 8.0, -8.0, -1, 49, 0, false, false, false)
-    Citizen.Wait(5000) -- Adjust the duration of the animation as needed
-
-    ClearPedTasks(playerPed)
-end
-
--- Function to give the player a jewel item
--- function GiveJewelItem(index)
---     local case = jewelCases[index]
---     case.looted = true
---     -- Trigger the server event to add the item and handle bank robbery
---     TriggerServerEvent('zaps:bankRobbery', 1000)
--- end
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
@@ -216,7 +120,7 @@ end)
 
 -- Function to give the player a jewel item
 function GiveJewelItem(index)
-    local case = jewelCases[index]
+    local case = Config.Framework.JewelCases[index]
     case.looted = true
 
     -- Trigger the server event to add the item and handle bank robbery
@@ -267,7 +171,6 @@ AddEventHandler('robberyFinished', function()
 
     -- Add any additional reset logic or actions you need
     commandString = 'heist'
-    -- Print a message to the player indicating the robbery has finished
     TriggerEvent("chatMessage", "^1Robbery finished.")
     ExecuteCommand(
 	commandString
@@ -284,8 +187,6 @@ Citizen.CreateThread(function()
         local playerCoords = GetEntityCoords(playerPed)
 
         if IsRobbing and #(playerCoords - vector3(-1238.2, -802.29, 17.84)) > 50 then
-            -- The player has walked more than 10 units away from the robbery coordinates
-            -- Finish the robbery
             TriggerEvent('robberyFinished')
         end
     end
