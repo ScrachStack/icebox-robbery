313d89e4f5634000eff293369c38caf1f8385601
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-30 02:41:21 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,11 +6,18 @@ Config.Framework = {
     CashToBeGiven = '',
     FrameworkType = '', -- 'qb', 'esx legacy', or 'nd'
     UseOxInventory = true, -- [true/false]
-    JewelCases = { -- this is how you can edit the coords there vector4
-        { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62, looted = false }, -- Case 01
-        { x = -1238.6, y = -804.88, z = 17.84, heading = 7.62, looted = false }, -- Case 02
-        { x = -1238.45, y = -806.59, z = 17.84, heading = 197.62, looted = false }, -- Case 03
+    JewelCases = {
+        { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62, looted = false },
+        { x = -1238.6, y = -804.88, z = 17.84, heading = 7.62, looted = false },
+        { x = -1238.45, y = -806.59, z = 17.84, heading = 197.62, looted = false },
+    },
 
-        -- Add more jewel case positions here
+    BLip = {
+        x = -1237.12,
+        y = -802.53,
+        z = 17.84,
+        sprite = 363,
+        color = 0,
+        name = "Icebox"
     }
 }
