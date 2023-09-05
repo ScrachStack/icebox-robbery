a5225df503690ecf7886fb3fd6b2f5555ee3dc8d
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-04 18:43:33 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,14 +3,32 @@ Config = {}
 Config.Framework = {
     ItemName = 'Jewel',
     Amount = '1',
-    CashToBeGiven = '',
-    FrameworkType = '', -- 'qb', 'esx legacy', or 'nd'
+    CashToBeGiven = '10000',
+    FrameworkType = 'esx', -- 'qb', 'esx legacy', or 'nd'
     UseOxInventory = true, -- [true/false]
     JewelCases = {
-        { x = -1238.66, y = -801.7, z = 17.84, heading = 130.62, looted = false },
-        { x = -1238.6, y = -804.88, z = 17.84, heading = 7.62, looted = false },
-        { x = -1238.45, y = -806.59, z = 17.84, heading = 197.62, looted = false },
-    },
+        { x = -1031.3530, y = 1489.4788, z = 19.6354, heading = 29.2036, looted = false }, -- Case 01
+    { x = -1034.6549, y = 1488.6283, z = 19.6354, heading = 169.6532, looted = false }, -- Case 02
+    { x = -1035.1873, y = 1490.7787, z = 19.6354, heading = 110.8922, looted = false }, -- Case 03
+    -- Add more jewel case positions here
+    { x = -1035.1855, y = 1490.7787, z = 19.6354, heading = 110.8922, looted = false }, -- Case 03
+
+    { x = -1035.4139, y =  1494.0825, z =  19.6354, heading = 83.7418, looted = false }, -- Case 03
+    { x = -1035.3044, y =  1496.0577, z =  19.6354, heading = 16.5188, looted = false }, -- Case 03
+    { x =   -1035.0918, y =  1497.5173, z =  19.6354, heading = 20.4643, looted = false }, -- Case 03
+    { x =   -1034.6624, y =  1499.8009, z =  19.6354, heading = 30.1808, looted = false }, -- Case 03
+    { x =   -1034.5095, y =  1501.6202, z =  19.6354, heading = 2.5372, looted = false }, -- Case 03
+    { x =   -1033.5786, y =  1502.4120, z =  19.6354, heading = 261.7649, looted = false }, -- Case 03
+    { x =   -1031.2445, y =  1504.4475, z =  19.6354, heading = 153.0843, looted = false }, -- Case 03
+    { x =   -1029.1541, y =  1503.1412, z =  19.6356, heading = 122.1328, looted = false }, -- Case 03
+    { x =   -1029.0790, y =  1501.1024, z =  19.6356, heading = 136.1882, looted = false }, -- Case 03
+    { x =   -1027.856, y =  1502.0404, z =  19.6357, heading = 302.3087, looted = false }, -- Case 03
+    { x =   -1027.9824, y =  1500.0612, z =  19.6357, heading = 288.6875, looted = false }, -- Case 03
+    { x =   -1028.4614, y =  1498.3534, z =  231.4505, heading = 288.6875, looted = false }, -- Case 03
+
+    -- -1028.4614, 1498.3534, 19.6357, 231.4505
+}
+    }
 
     BLip = {
         x = -1237.12,
@@ -20,4 +38,3 @@ Config.Framework = {
         color = 0,
         name = "Icebox"
     }
-}
