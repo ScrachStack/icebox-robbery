6a4bb62e27d377856d7aa72c3405966fb3ac593f
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-11 14:03:54 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,13 +6,16 @@ Config.Framework = {
     CashToBeGiven = '10000',
     FrameworkType = 'esx', -- 'qb', 'esx legacy', or 'nd'
     UseOxInventory = true, -- [true/false]
+     Marker = {
+        x = -1256.96,
+        y = -812.98,
+        z = 17.84
+    },
     JewelCases = {
-        { x = -1031.3530, y = 1489.4788, z = 19.6354, heading = 29.2036, looted = false }, -- Case 01
+    { x = -1031.3530, y = 1489.4788, z = 19.6354, heading = 29.2036, looted = false }, -- Case 01
     { x = -1034.6549, y = 1488.6283, z = 19.6354, heading = 169.6532, looted = false }, -- Case 02
     { x = -1035.1873, y = 1490.7787, z = 19.6354, heading = 110.8922, looted = false }, -- Case 03
-    -- Add more jewel case positions here
     { x = -1035.1855, y = 1490.7787, z = 19.6354, heading = 110.8922, looted = false }, -- Case 03
-
     { x = -1035.4139, y =  1494.0825, z =  19.6354, heading = 83.7418, looted = false }, -- Case 03
     { x = -1035.3044, y =  1496.0577, z =  19.6354, heading = 16.5188, looted = false }, -- Case 03
     { x =   -1035.0918, y =  1497.5173, z =  19.6354, heading = 20.4643, looted = false }, -- Case 03
@@ -25,8 +28,6 @@ Config.Framework = {
     { x =   -1027.856, y =  1502.0404, z =  19.6357, heading = 302.3087, looted = false }, -- Case 03
     { x =   -1027.9824, y =  1500.0612, z =  19.6357, heading = 288.6875, looted = false }, -- Case 03
     { x =   -1028.4614, y =  1498.3534, z =  231.4505, heading = 288.6875, looted = false }, -- Case 03
-
-    -- -1028.4614, 1498.3534, 19.6357, 231.4505
 }
     }
 
